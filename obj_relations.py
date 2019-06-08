6f47e4da75acd64a976569e64e8ed422f9ace18a
geehad <gehadmohsen9519@gmail.com>
2019-06-08 21:36:21 +0200

    obj_relations

--- obj_relations.py
+++ obj_relations.py
@@ -3,34 +3,52 @@ from spacy import displacy
 
 
 nlp = spacy.load('en')
-doc = nlp("The cup which is blue is on the table")
+
+#doc = nlp("The cup is on the table and under the lamp and in the room")
+#doc = nlp("The cup is on the table and under the lamp and in the room and a chair behind a table")
+#doc = nlp("There is a cup on the table and a book in the room and a chair behind a table")
+#doc = nlp("The present inside the big box is red")
+#doc = nlp("The cup which is blue is on the table")
 #doc = nlp("The cup Ahmed put is on the table")
-########################doc = nlp("The cup which is blue is on the table and under the lamp")
-########################doc = nlp("The present inside the big box is mine")
+doc = nlp("The boy was at his home")
+#doc = nlp("The cup which is blue is on the table and under the lamp")
 
 relations = []
 
 for word in doc:
-    prep=pnoun=pobj = " "
+    current_prep=pnoun=pobj = " "
     if word.pos_ == "ADP":
-        prep = str(word)
-        children = word.children
-        for child in children:
+
+        current_prep = str(word)
+
+        #################################### find pnoun #########################################
+        current_word = word
+        while current_word.head.pos_ == "ADP":
+            current_word = current_word.head
+            print("Enter",str(current_word))
+        if current_word.head.pos_ == "VERB":
+            print("VERB",str(current_word.head))
+            children_verb = current_word.head.children
+            for child in children_verb:
+                if child.dep_ == "nsubj":
+                    pnoun = str(child)
+        else:
+            print("not Verb")
+            pnoun = str(current_word.head)
+
+        #########################################################################################
+
+        #################################  find pobj ############################################
+        for child in word.children:
             if child.dep_ == "pobj":
                 pobj = str(child)
+                rel = (current_prep, pnoun, pobj)
+                relations.append(rel)
 
-        word_head = word.head
-        children_verb = word_head.children
-        for child in children_verb:
-            if child.dep_ == "nsubj":
-                pnoun = str(child)
+        ###########################################################################################
 
-
-    if prep != " ":
-        rel = (prep,pnoun,pobj)
-        relations.append(rel)
 
 print(relations)
 
-displacy.serve(doc,style='dep')
+#displacy.serve(doc,style='dep')
 
