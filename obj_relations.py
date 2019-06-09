eb4d9fd14b63e9734680e9170e2ddca4e4fd9a19
geehad <gehadmohsen9519@gmail.com>
2019-06-09 22:14:54 +0200

    obj_relations

--- obj_relations.py
+++ obj_relations.py
@@ -7,11 +7,22 @@ nlp = spacy.load('en')
 #doc = nlp("The cup is on the table and under the lamp and in the room")
 #doc = nlp("The cup is on the table and under the lamp and in the room and a chair behind a table")
 #doc = nlp("There is a cup on the table and a book in the room and a chair behind a table")
-#doc = nlp("The present inside the big box is red")
 #doc = nlp("The cup which is blue is on the table")
 #doc = nlp("The cup Ahmed put is on the table")
-doc = nlp("The boy was at his home")
+#doc = nlp("The boy was at his home")
 #doc = nlp("The cup which is blue is on the table and under the lamp")
+#doc = nlp("The table is opposite the door")
+#doc = nlp("The boy is right of the table")
+#doc = nlp("The boy was setting on the chair")
+#doc = nlp("The boy placed his book on the table")
+#doc = nlp("There is a cat under the red table")
+#doc = nlp("The present inside the big box is red")
+#doc = nlp("The boy was to the left of the table")
+
+#doc = nlp("The car is parked in front of the garden")
+doc = nlp("There is a red chair to the right of the table")
+
+
 
 relations = []
 
@@ -23,14 +34,18 @@ for word in doc:
 
         #################################### find pnoun #########################################
         current_word = word
-        while current_word.head.pos_ == "ADP":
-            current_word = current_word.head
-            print("Enter",str(current_word))
+        while current_word.head.pos_ == "ADP" or (current_word.head.pos_ == "NOUN" and current_word.head.dep_ == "pobj"):
+                if (current_word.head.pos_ == "NOUN" and current_word.head.dep_ == "pobj"):
+                    current_prep = str(current_word.head)
+
+                current_word = current_word.head
+                print("Enter", word,str(current_word))
+
         if current_word.head.pos_ == "VERB":
             print("VERB",str(current_word.head))
             children_verb = current_word.head.children
             for child in children_verb:
-                if child.dep_ == "nsubj":
+                if child.dep_ == "nsubj" or child.dep_ == "nsubjpass":
                     pnoun = str(child)
         else:
             print("not Verb")
@@ -41,14 +56,22 @@ for word in doc:
         #################################  find pobj ############################################
         for child in word.children:
             if child.dep_ == "pobj":
-                pobj = str(child)
-                rel = (current_prep, pnoun, pobj)
-                relations.append(rel)
+                child_refer_to_prep = False
+                for Child in child.children:
+                    if Child.dep_ == "prep":
+                        child_refer_to_prep = True
+                        break
+                if not child_refer_to_prep:
+                    print("push" , word)
+                    pobj = str(child)
+                    rel = (current_prep, pnoun, pobj)
+                    relations.append(rel)
+
 
         ###########################################################################################
 
 
 print(relations)
 
-#displacy.serve(doc,style='dep')
+displacy.serve(doc,style='dep')
 
