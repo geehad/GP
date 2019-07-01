96515e3761dd9112f360c3b9502db6ff20854299
geehad <gehadmohsen9519@gmail.com>
2019-07-01 23:21:29 +0200

    NLP_module

--- NLP_module.py
+++ NLP_module.py
@@ -23,14 +23,35 @@ models_info=extract_models_char(input_text)
 relations_models=Objs_relations(input_text,models_info)
 model_actions =extract_models_actions(input_text,models_info)
 sequence(input_text)
-########################### write model chars ######################
 
+########################### write model chars ######################
 for i in range(0,len(models_info)):
     current_model_name = models_info[i][1]
     current_model_chars = models_info[i][2]
     file_models_char.write(current_model_name + " ")
-    for j in range(0, len(current_model_chars)):
-        file_models_char.write(str(current_model_chars[j]) + " ")
+
+
+
+    if len(current_model_chars) == 3:  #human
+        for j in range(0, len(current_model_chars)-1):
+            if j == 0 and current_model_chars[j] == -1:    #not mentioned age then set it not old
+                current_model_chars[j] = 0
+                file_models_char.write(str(current_model_chars[j]) + " ")
+            elif j == 2 and current_model_chars[j] == -1: #not mentioned height then set it meduim height
+                current_model_chars[j] = 1
+                file_models_char.write(str(current_model_chars[j]) + " ")
+            else:
+                file_models_char.write(str(current_model_chars[j]) + " ")
+
+
+    else:
+        for j in range(0, len(current_model_chars)-1):
+            if j == 1 and current_model_chars[j] == -1:  # not mentioned size then set it meduim size
+                current_model_chars[j] = 1
+                file_models_char.write(str(current_model_chars[j]) + " ")
+            else:
+                file_models_char.write(str(current_model_chars[j]) + " ")
+
 
     file_models_char.write(str(models_info[i][3]) + "\n")
 
